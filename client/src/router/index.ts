1fdb30c7023279d53f0fb55ae8109e624e4d0e01
Yeami <ivanov.nikita@yeami.group>
2020-08-07 20:18:03 +0300

    Implement the basic routing system

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -1,22 +1,39 @@
 import Vue from 'vue';
 import VueRouter, { RouteConfig } from 'vue-router';
-import Home from '../views/Home.vue';
+
+import { isAuthenticated, isNotAuthenticated } from '@/services/auth.service';
+import TheLoginPage from '@/views/TheLoginPage.vue';
+import TheProfilePage from '@/views/TheProfilePage.vue';
+import TheRouterView from '@/views/TheRouterView.vue';
 
 Vue.use(VueRouter);
 
 const routes: Array<RouteConfig> = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home,
+    path: '/login',
+    name: 'login',
+    component: TheLoginPage,
+    beforeEnter: (to, from, next) => isNotAuthenticated(to, from, next),
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+    path: '/',
+    redirect: '/profile',
+  },
+  {
+    path: '/',
+    component: TheRouterView,
+    beforeEnter: (to, from, next) => isAuthenticated(to, from, next),
+    children: [
+      {
+        path: 'profile',
+        name: 'profile',
+        component: TheProfilePage,
+      },
+    ],
+  },
+  {
+    path: '/*',
+    redirect: '/profile',
   },
 ];
 
