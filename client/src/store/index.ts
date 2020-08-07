b2ed010f6574bf363da4a5ef24b79b5546f6df94
Yeami <ivanov.nikita@yeami.group>
2020-08-07 20:17:16 +0300

    Implement the basic user store

--- client/src/store/index.ts
+++ client/src/store/index.ts
@@ -1,15 +1,12 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 
+import user from '@/store/user/user.module';
+
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
   modules: {
+    user,
   },
 });
