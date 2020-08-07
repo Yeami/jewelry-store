0a199d8c31f5ab77fb0c4f4782ca4a70f442a509
Yeami <ivanov.nikita@yeami.group>
2020-08-07 20:06:09 +0300

    Update the auth user manager functionality to work with usernames

--- server/api/managers.py
+++ server/api/managers.py
@@ -3,18 +3,29 @@ from django.contrib.auth.base_user import BaseUserManager
 
 class CustomUserManager(BaseUserManager):
     """
-    Custom user model manager where email is the unique identifiers
-    for authentication instead of usernames.
+    Custom user model manager where username is the unique identifiers for authentication.
     """
-    def create_user(self, email, password, **extra_fields):
+    def create_user(self, username, email, password, **extra_fields):
         """
-        Create and save a User with the given email and password.
+        Create and save a User with the given username, email and password.
         """
-        if not email:
-            raise ValueError('The Email must be set')
+        if not username:
+            raise ValueError('The username must be set')
         email = self.normalize_email(email)
-        user = self.model(email=email, **extra_fields)
+        username = self.model.normalize_username(username)
+        user = self.model(username=username, email=email, **extra_fields)
         user.set_password(password)
         user.save()
 
         return user
+
+    def create_superuser(self, username, email, password, **extra_fields):
+        """
+        Create and save a SuperUser with the given username, email and password.
+        """
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('role_id', 3)
+
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+        return self.create_user(username, email, password, **extra_fields)
