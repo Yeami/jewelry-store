cba63b6152f530c64fec74095baef06ce5617bca
Yeami <ivanov.nikita@yeami.group>
2020-08-11 13:45:09 +0300

    Implement the getting brands list functionality

--- client/src/views/TheProductsPage.vue
+++ client/src/views/TheProductsPage.vue
@@ -1,10 +1,24 @@
 <template>
-  <h1>Products page</h1>
+  <div class="products-page-wrapper">
+    <h1>Products page</h1>
+  </div>
 </template>
 
 <script>
+import { mapGetters } from 'vuex';
+
+import { GET_BRANDS } from '@/store/actions.type';
+
 export default {
   name: 'TheProductsPage',
+  computed: {
+    ...mapGetters([
+      'brands',
+    ]),
+  },
+  mounted() {
+    this.$store.dispatch(GET_BRANDS);
+  },
 };
 </script>
 
