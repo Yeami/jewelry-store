ca930aeaa1cc71d215ad4f6f486f1ac37fed0129
Yeami <ivanov.nikita@yeami.group>
2020-08-11 13:55:03 +0300

    Implement the categories' display functionality

--- client/src/views/TheProductsPage.vue
+++ client/src/views/TheProductsPage.vue
@@ -1,23 +1,41 @@
 <template>
   <div class="products-page-wrapper">
     <h1>Products page</h1>
+
+    <div class="content">
+      <a-tabs default-active-key="1" tab-position="left" v-if="categories">
+        <a-tab-pane key="0" tab="All">
+          Content of All
+        </a-tab-pane>
+        <a-tab-pane
+            v-for="item in categories"
+            :key="item.id"
+            :tab="item.name"
+        >
+          {{item.description}}
+        </a-tab-pane>
+      </a-tabs>
+    </div>
+
   </div>
 </template>
 
 <script>
 import { mapGetters } from 'vuex';
 
-import { GET_BRANDS } from '@/store/actions.type';
+import { GET_BRANDS, GET_CATEGORIES } from '@/store/actions.type';
 
 export default {
   name: 'TheProductsPage',
   computed: {
     ...mapGetters([
       'brands',
+      'categories',
     ]),
   },
   mounted() {
     this.$store.dispatch(GET_BRANDS);
+    this.$store.dispatch(GET_CATEGORIES);
   },
 };
 </script>
