2e0e0f7c1fcb8f54a551a7c28b8859c747a4babe
Yeami <ivanov.nikita@yeami.group>
2020-08-12 16:14:01 +0300

    Update the product model migration

--- server/api/migrations/0004_product.py
+++ server/api/migrations/0004_product.py
@@ -15,13 +15,14 @@ class Migration(migrations.Migration):
             name='Product',
             fields=[
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('name', models.CharField(max_length=40, unique=True)),
+                ('name', models.TextField(unique=True)),
                 ('description', models.TextField(blank=True)),
                 ('price', models.CharField(max_length=20)),
                 ('amount', models.IntegerField(default=0)),
                 ('is_available', models.BooleanField(default=False)),
                 ('brand', models.ForeignKey(on_delete=django.db.models.deletion.DO_NOTHING, to='api.Brand')),
                 ('category', models.ForeignKey(on_delete=django.db.models.deletion.DO_NOTHING, to='api.Category')),
+                ('picture', models.TextField(blank=True)),
             ],
             options={
                 'db_table': 'product',
